bc59049ca5ac0a184d8548c8e608bd661168ae0f
Shanmugam37 <99902963+Shanmugam37@users.noreply.github.com>
2023-05-30 22:29:09 +0530

    jenk

---
+++ maven-integration-shannu-master/src/main/java/jenkins/Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("new file");
+		submit();
+	}
+	public static void submit()
+	{
+		System.out.println("hiii");
+	}
+} 
